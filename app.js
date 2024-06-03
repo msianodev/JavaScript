ba411fd5447e585c63c9f612312ec52c4543f9cf
msianodev <matias.siano@outlook.com>
2024-06-03 15:20:49 -0300

    primeros-pasos

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+    tipos de variables primitivas
+*/
+let numero = 10; // variable tipo number
+let nombre = 'Matias'; // variable tipo string
+let verdadero = true; // variable tipo boolean
+let undef; // variable tipo undefined
+let nulo = null; // variable tipo null
+
+function sumar(a, b) { // funcion que suma dos numeros
+    return a + b;
+}
+
+const numeroPi = 3.1416; // variable constante
+
+let mascota = {}; // variable tipo objeto
+
+console.log('Hola Mundo'); // imprimir en consola
+console.log(typeof mascota); // imprimir el tipo de variable
